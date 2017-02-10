6a03c1faebb8bc83e37f3ed3f2dd13f3d0771894
sebasvega95 <sebas060495@gmail.com>
2017-02-10 10:58:04 -0500

    Scaffolding for requests and moved stuff around

--- file-system/client.cpp
+++ file-system/client.cpp
@@ -1,35 +1,165 @@
 #include <bits/stdc++.h>
 #include <zmqpp/zmqpp.hpp>
-#include "json.hpp"
-#include "base64.hpp"
+#include "lib/json.hpp"
+#include "lib/base64.hpp"
+
+#define NAME_REQ 0
+#define LS_REQ 1
+#define RD_REQ 2
+#define CR_REQ 3
+#define RM_REQ 4
 
 using namespace std;
 using namespace zmqpp;
 using json = nlohmann::json;
 
-int main(int argc, char const *argv[]) {
-  context ctx;
-  socket s(ctx, socket_type::req);
-  s.connect("tcp://localhost:5555");
+void Pause() {
+  cout << "Press any key to continue...";
+  cin.ignore().get();
+}
 
+string GetName(socket& s) {
+  regex r("[a-zA-Z]\\w*");
+  string name;
+
+  bool ok;
+  do {
+    system("clear");
+    cout << "Username: ";
+    getline(cin, name);
+    json req;
+    req["type"] = NAME_REQ;
+    req["user"] = name;
+
+    message m;
+    m << req.dump();
+    s.send(m);
+
+    message ans;
+    s.receive(ans);
+    string _res;
+    ans >> _res;
+    json res = json::parse(_res);
+    cout << res.dump(2) << endl;
+    ok = res["res"] == "OK";
+    if (!ok) {
+      cout << "Invalid name" << endl;
+      Pause();
+    }
+  } while(!ok);
+
+  return name;
+}
+
+int GetOption(string& opt) {
+  stringstream ss(opt);
+  int o;
+  ss >> o;
+
+  if (!ss)
+    return -1;
+  return o;
+}
+
+void ListFiles(string &user, socket& s) {
   json load;
-  load["user"] = "aerendon";
-  load["name"] = "copy.png";
-
-
-  ifstream fin("mushroom.png", ios::binary);
-  stringstream oss;
-  oss << fin.rdbuf();
-
-  Base64 b64;
-  string in = oss.str(), out;
-  b64.Encode(in, &out);
-  load["file"] = out;
+  load["type"] = LS_REQ;
+  load["user"] = user;
 
   message m;
   m << load.dump();
   s.send(m);
-  fin.close();
+
+  message ans;
+  s.receive(ans);
+  string _res;
+  ans >> _res;
+  json res = json::parse(_res);
+
+  for (auto &f : res["data"]) {
+    cout << f << endl;
+  }
+}
+
+bool ExecuteOpt(int opt, string &user, socket& s) {
+  switch (opt) {
+    case LS_REQ:
+      // ListFiles(user, s);
+      break;
+    case RD_REQ:
+      break;
+    case CR_REQ:
+      break;
+    case RM_REQ:
+     break;
+    case 0:
+      return false;
+      break;
+    default:
+      cout << "Invalid option" << endl;
+      Pause();
+      break;
+  }
+
+  return true;
+}
+
+bool PrintMenu(string &user, socket& s) {
+  system("clear");
+
+  cout << "Welcome " << user << " to SuperFancy FileSystem (SFFS)" << endl;
+  cout << "Please enter your choice :)" << endl;
+  cout << "1. List your files" << endl;
+  cout << "2. Get a file" << endl;
+  cout << "3. Upload a file" << endl;
+  cout << "4. Delete a file" << endl;
+  cout << "0. Exit" << endl;
+  cout << "> ";
+
+  string _opt;
+  cin >> _opt;
+  int opt = GetOption(_opt);
+  return ExecuteOpt(opt, user, s);
+}
+
+int main(int argc, const char *argv[]) {
+  string ip = "localhost", port = "5555";
+  context ctx;
+  socket s(ctx, socket_type::req);
+  s.connect("tcp://" + ip + ":" + port);
+
+  string user = GetName(s);
+
+  bool cont;
+  do {
+    cont = PrintMenu(user, s);
+  } while(cont);
+
+  // context ctx;
+  // socket s(ctx, socket_type::req);
+  // s.connect("tcp://localhost:5555");
+  //
+  // json load;
+  // load["user"] = "leiver";
+  // load["name"] = "copy.mp3";
+  //
+  // ifstream fin("fs/Super Mario World Music - Title Theme.mp3", ios::binary);
+  // if (!fin) {
+  //   cout << "Mucha loca" << endl;
+  //   return 0;
+  // }
+  // stringstream oss;
+  // oss << fin.rdbuf();
+  // fin.close();
+  //
+  // Base64 b64;
+  // string in = oss.str(), out;
+  // b64.Encode(in, &out);
+  // load["file"] = out;
+  //
+  // message m;
+  // m << load.dump();
+  // s.send(m);
 
   return 0;
 }
--- file-system/server.cpp
+++ file-system/server.cpp
@@ -1,37 +1,119 @@
 #include <bits/stdc++.h>
 #include <zmqpp/zmqpp.hpp>
-#include "json.hpp"
-#include "base64.hpp"
+#include "lib/json.hpp"
+#include "lib/base64.hpp"
+
+#define NAME_REQ 0
+#define LS_REQ 1
+#define RD_REQ 2
+#define CR_REQ 3
+#define RM_REQ 4
 
 using namespace std;
 using namespace zmqpp;
 using json = nlohmann::json;
 
-int main(int argc, char const *argv[]) {
+json db;
+
+void InitDb() {
+  ifstream fin("db.json");
+  fin >> db;
+  fin.close();
+}
+
+void UpdateDb() {
+  ofstream fout("db.json");
+  fout << setw(2) << db << endl;
+  fout.close();
+}
+
+void InitUser(string& user, socket &s) {
+  json res;
+  cout << res.dump(2) << endl;
+
+  if (db[user]) {
+    res["res"] = "OK";
+  } else {
+    string user;
+    regex r("[a-zA-Z]\\w*");
+    if (regex_match(user, r)) {
+      db[user] = json::array();
+      system(("mkdir fs/" + user).c_str());
+      res["res"] = "OK";
+      UpdateDb();
+    } else {
+      res["res"] = "Invalid name";
+    }
+  }
+
+  message ans;
+  ans << res.dump();
+  s.send(ans);
+}
+
+void Serve(int opt, string& user, socket& s) {
+  switch (opt) {
+    case NAME_REQ:
+      InitUser(user, s);
+      break;
+    case LS_REQ:
+      break;
+    case RD_REQ:
+      break;
+    case CR_REQ:
+      break;
+    case RM_REQ:
+     break;
+    default:
+      break;
+  }
+}
+
+void GetReq(socket& s) {
+  message m;
+  s.receive(m);
+  string _req;
+  m >> _req;
+  json req = json::parse(_req);
+  cout << req.dump(2) << endl;
+
+  int opt = req["type"];
+  string user = req["user"];
+  Serve(opt, user, s);
+}
+
+int main(int argc, const char *argv[]) {
+  cout << "Init DB" << endl;
+  InitDb();
+
   context ctx;
   socket s(ctx, socket_type::rep);
   s.bind("tcp://*:5555");
 
-  cout << "Waiting for message..." << endl;
-  message m;
-  s.receive(m);
-  cout << "Message received!" << endl;
+  cout << "Waiting for requests" << endl;
+  while (true) {
+    GetReq(s);
+  }
 
-  string text;
-  m >> text;
-  cout << text << endl;
-  auto r = json::parse(text);
-
-  Base64 b64;
-  string in = r["file"], out;
-  b64.Decode(in, &out);
-
-  string filename = r["name"];
-  ofstream fout(filename);
-  fout << out;
-  fout.close();
-
-  cout << "File written!" << endl;
+  // cout << "Waiting for message..." << endl;
+  // message m;
+  // s.receive(m);
+  // cout << "Message received!" << endl;
+  //
+  // string text;
+  // m >> text;
+  // auto r = json::parse(text);
+  //
+  // Base64 b64;
+  // string in = r["file"], out;
+  // b64.Decode(in, &out);
+  //
+  // string filename = r["name"];
+  // ofstream fout("fs/" + filename);
+  // fout << out;
+  // fout.close();
+  //
+  // cout << "File written!" << endl;
 
   return 0;
 }
