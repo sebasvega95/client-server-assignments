4e2296f72a113b88bbe23e6dea7c3d55f9fad05b
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-02-17 13:22:09 -0500

    added send file by chunks

--- file-system-v2.0/lib/constants.hpp
+++ file-system-v2.0/lib/constants.hpp
@@ -6,5 +6,6 @@ const int LS_REQ = 1;
 const int GET_REQ = 2;
 const int SEND_REQ = 3;
 const int RM_REQ = 4;
+const int CHUNK_SIZE = 524288; // 512 KiB
 
 #endif
--- file-system-v2.0/lib/file.hpp
+++ file-system-v2.0/lib/file.hpp
@@ -2,40 +2,95 @@
 #define FILE_HPP
 
 #include <bits/stdc++.h>
+#include "constants.hpp"
 #include "base64.hpp"
 #define FILE_NOT_FOUND "@"
 
 using namespace std;
 
-string ReadFileBase64(string& filename) {
+class ReadFileBase64 {
+private:
+  string filename;
+  int cur_pos;
+  bool finished;
+public:
+  ReadFileBase64(string _filename) : filename(_filename) {}
+
+  string operator()() {
     ifstream fin(filename, ios::binary);
     if (!fin) {
-        return FILE_NOT_FOUND;
+      return FILE_NOT_FOUND;
     }
 
-    stringstream oss;
-    oss << fin.rdbuf();
-    fin.close();
+    char *buffer = new char[CHUNK_SIZE];
+    fin.seekg(cur_pos);
+    fin.read(buffer, CHUNK_SIZE);
+    cur_pos += CHUNK_SIZE;
+    finished = (bool) fin;
 
-    Base64 b64;
-    string in = oss.str(), out;
-    b64.Encode(in, &out);
-
-    return out;
-}
-
-bool SaveFileBase64(string& filename, string& file) {
     Base64 b64;
     string out;
-    b64.Decode(file, &out);
-    ofstream fout(filename);
-    if (!fout) {
-        return false;
-    }
+    b64.Encode(buffer, &out);
 
-    fout << out;
-    fout.close();
-    return true;
-}
+    return out;
+  }
+
+  bool is_finished() {
+    return finished;
+  }
+};
+
+class SaveFileBase64 {
+private:
+  string filename;
+  bool is_first;
+  public:
+    SaveFileBase64(string _filename) : filename(_filename), is_first(true) {}
+
+    void operator()() {
+      Base64 b64;
+      string out;
+      b64.Decode(file, &out);
+      ofstream fout(filename);
+      if (!fout) {
+        return false;
+      }
+
+      fout << out;
+      fout.close();
+      return true;
+    }
+};
+
+// string ReadFileBase64(string& filename) {
+//     ifstream fin(filename, ios::binary);
+//     if (!fin) {
+//         return FILE_NOT_FOUND;
+//     }
+//
+//     stringstream oss;
+//     oss << fin.rdbuf();
+//     fin.close();
+//
+//     Base64 b64;
+//     string in = oss.str(), out;
+//     b64.Encode(in, &out);
+//
+//     return out;
+// }
+
+// bool SaveFileBase64(string& filename, string& file) {
+//     Base64 b64;
+//     string out;
+//     b64.Decode(file, &out);
+//     ofstream fout(filename);
+//     if (!fout) {
+//         return false;
+//     }
+//
+//     fout << out;
+//     fout.close();
+//     return true;
+// }
 
 #endif
